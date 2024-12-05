fbf8627f93692f1c82c0cd692e78852092d68ff6
devprincess <princess.solomon@eng.uniben.edu>
2024-12-05 09:00:42 +0100

    added color properties and fixed decimal

--- encap.js
+++ encap.js
@@ -1,50 +1,56 @@
 //intoducing the SportCar parent class
 class SportCar{
-    constructor(make, model, topSpeed, size){
+    constructor(make, model, topSpeed){
         this.make = make;
         this.model = model;
         this.topSpeed = topSpeed;
     }
 
-//how these properties would be displayed
-displayDetails() {
-    console.log(`SportsCar: ${this.make} ${this.model}, Top Speed: ${this.topSpeed} km/h`);
-}
+    //how these properties would be displayed
+    displayDetails() {
+        console.log(`SportsCar: ${this.make} ${this.model}, Top Speed: ${this.topSpeed} km/h`);
+    }
 
 
-//the method use to calculate speed of car
-timeToTopSpeed(acceleration){
-    return this.topSpeed / acceleration;
-}
+    //the method use to calculate speed of car
+    timeToTopSpeed(acceleration){
+        return this.topSpeed / acceleration;
+    }
+
+    //the conversion to two decimal place
+    convertToDecimal(speed){
+        return speed .toFixed(2);
+    }
 
 }
 
 //introducing our Child class
 class LuxuryCar extends SportCar{
-    constructor(make, model, topSpeed, luxuryFeatures){
-        super(make, model, topSpeed, luxuryFeatures)
+    constructor(make, model, topSpeed, color, luxuryFeatures){
+        super(make, model, topSpeed, color, luxuryFeatures) 
             this.luxuryFeatures = luxuryFeatures;   
+            this.color = color;
     }
 
-//the properties to display
+    //the properties to display
 
-displayDetails() {
-    console.log(`LuxuryCar: ${this.make} ${this.model}, Top Speed: ${this.topSpeed} km/h`);
-    console.log(`Luxury Features: ${this.luxuryFeatures.join(', ')}`);
+    displayDetails() {
+        console.log(`LuxuryCar: ${this.make} ${this.model}, Top Speed: ${this.topSpeed} km/h`);
+        console.log(`Luxury Features: ${this.luxuryFeatures.join(', ')}, Color: ${this.color}`);
+    }
+
+    playAmbientLighting() {
+        console.log(`${this.make} ${this.model} is playing ambient lighting to set the mood.`);
+    }
 }
 
-playAmbientLighting() {
-    console.log(`${this.make} ${this.model} is playing ambient lighting to set the mood.`);
-}
-
-}
 //getting the new sportcar
 const ferrari = new SportCar("Ferrari", "488 Spider", 330);
 ferrari.displayDetails();
-console.log(`Time to top speed: ${ferrari.timeToTopSpeed(50)} seconds`);
+console.log(`Time to top speed: ${ferrari.convertToDecimal(ferrari.timeToTopSpeed(50))} seconds`);
 
 //display the new sportcar
-const rollsRoyce = new LuxuryCar("Rolls Royce", "Phantom", 250, ["Massage Seats", "Rear Entertainment", "Ambient Lighting."]);
+const rollsRoyce = new LuxuryCar("Rolls Royce", "Phantom", 250, "red", ["Massage Seats", "Rear Entertainment", "Ambient Lighting."]);
 rollsRoyce.displayDetails();
-console.log(`Time to top speed: ${rollsRoyce.timeToTopSpeed(30)} seconds`);
+console.log(`Time to top speed: ${rollsRoyce.convertToDecimal(rollsRoyce.timeToTopSpeed(30))} seconds`);
 rollsRoyce.playAmbientLighting();
